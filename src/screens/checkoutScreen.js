09eb8baf87db731b44f560455aae1ff2bceb3824
jana <janagh2004@gmail.com>
2025-08-24 09:51:06 +0300

    fix + screenshots

--- src/screens/checkoutScreen.js
+++ src/screens/checkoutScreen.js
@@ -27,7 +27,7 @@ export default function CheckOutScreen({ route }) {
     return (
         <View>
             <View style={{ padding: 20 }}>
-                <HeaderCard headerTitle= {t("checkout")} isHome={false} />
+                <HeaderCard headerTitle={t("checkout")} isHome={false} />
 
                 <View style={{
                     alignItems: "center",
@@ -36,7 +36,8 @@ export default function CheckOutScreen({ route }) {
                     width: 80,
                     height: 80,
                     borderRadius: 40,
-                    elevation: 5
+                    elevation: 5,
+
                 }}>
                     <Text style={{ fontSize: 60, color: "white" }}>↓</Text>
                 </View>
@@ -91,7 +92,7 @@ export default function CheckOutScreen({ route }) {
                 </View>
 
                 <View style={styles.delieveryCard}>
-                    
+
                     <Text style={styles.totalTitle}>
                         TOTAL:
                     </Text>
@@ -102,34 +103,42 @@ export default function CheckOutScreen({ route }) {
 
                 </View>
             </View >
-
-            <View
-                style={{
-                    flexDirection: "column",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    gap: 0
-                }}
-            >
+            <View>
                 <Svg
-                    height={200}
-                    width={Dimensions.get('screen').width}
+                    height={100}   
+                    width={Dimensions.get("screen").width}
                     viewBox="0 0 1440 320"
+                    style={{ marginBottom: -5 }}
                 >
                     <Path
                         fill='#002881'
-                        d="M0,96L18.5,122.7C36.9,149,74,203,111,197.3C147.7,192,185,128,222,96C258.5,64,295,64,332,58.7C369.2,53,406,43,443,85.3C480,128,517,224,554,234.7C590.8,245,628,171,665,122.7C701.5,75,738,53,775,53.3C812.3,53,849,75,886,117.3C923.1,160,960,224,997,218.7C1033.8,213,1071,139,1108,122.7C1144.6,107,1182,149,1218,170.7C1255.4,192,1292,192,1329,170.7C1366.2,149,1403,107,1422,85.3L1440,64L1440,320L1421.5,320C1403.1,320,1366,320,1329,320C1292.3,320,1255,320,1218,320C1181.5,320,1145,320,1108,320C1070.8,320,1034,320,997,320C960,320,923,320,886,320C849.2,320,812,320,775,320C738.5,320,702,320,665,320C627.7,320,591,320,554,320C516.9,320,480,320,443,320C406.2,320,369,320,332,320C295.4,320,258,320,222,320C184.6,320,148,320,111,320C73.8,320,37,320,18,320L0,320Z"
-
+                        d="M0,96L18.5,122.7C36.9,149,74,203,111,197.3C147.7,192,185,128,222,96C258.5,64,295,64,332,58.7C369.2,53,406,43,443,85.3C480,128,517,224,554,234.7C590.8,245,628,171,665,122.7C701.5,75,738,53,775,53.3C812.3,53,849,75,886,117.3C923.1,160,960,224,997,218.7C1033.8,213,1071,139,1108,122.7C1144.6,107,1182,149,1218,170.7C1255.4,192,1292,192,1329,170.7C1366.2,149,1403,107,1422,85.3L1440,64L1440,320L0,320Z"
                     />
                 </Svg>
 
-                <Image
-                    source={require('../../assets/thanku.png')}
-                    width={Dimensions.get('screen').width}
-                    height={100}
-                    resizeMode="contain"
-                />
+                <View
+                    style={{
+                        backgroundColor: "#002881",
+                        width: "100%",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingVertical: 30,
+                        marginTop: -5,   // <-- overlaps to hide the gap
+                        
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: "white",
+                            fontSize: 20,
+                            fontWeight: "bold",
+                        }}
+                    >
+                        THANK YOU :) !
+                    </Text>
+                </View>
             </View>
+
         </View >
     );
 }
